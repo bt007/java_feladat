03038315db6ea82a0e2228ab7e5ce1132928a572
unknown <ballatamas92@gmail.com>
2023-05-11 14:55:46 +0200

    Faktoriális tesztelés

--- Nap_0511/src/com/feladat/Matematika.java
+++ Nap_0511/src/com/feladat/Matematika.java
@@ -3,15 +3,26 @@ package com.feladat;
 public class Matematika {
     /**
      * N faktoriális számítása.
-     * @param n
+     * @param n aminek a faktoriálást számítani szeretnénk
      * @return n! értéke
      */
     public static long fakt(int n){
-        if(n == 0) return  1;
-        long f = 0;
+        if(n < 0){
+            throw new IllegalArgumentException("N értéke nem lehet negatív!"); // T1
+        }
 
-        for(int i = 0; i <= n; i++) {
-            f += i;
+        if(n == 0) return  1;
+        // T3 alapján 3 módosítás (+ helyett *, f és i kezdőértéke 1)
+        long f = 1;
+        long ft = 1; // T4 alapján túlcsordulás detektálása
+
+        for(int i = 1; i <= n; i++) {
+            ft = f;
+            f *= i;
+
+            if(f < ft){
+                throw new RuntimeException("A számítás során túlcsordulási hiba keletkezett!");
+            }
         }
         return f;
 
